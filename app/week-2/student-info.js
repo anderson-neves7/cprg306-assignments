e10a2a65c47393d05c5e1ff28b08bf9a93737d13
anderson-neves7 <anderson.neves@edu.sait.ca>
2025-09-19 22:50:06 -0600

    Complete Week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Anderson</p>
+      <Link href="https://github.com/anderson-neves7/cprg306-assignments">
+        My GitHub Repository
+      </Link>
+    </div>
+  );
+}
