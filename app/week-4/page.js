287873b77ca2d65f7d00e3ad272e4a11de38b07e
anderson-neves7 <anderson.neves@edu.sait.ca>
2025-10-03 22:07:09 -0600

    Created Week 4 folder and corresponding files

---
+++ app/week-4/page.js
@@ -0,0 +1,9 @@
+import NewItem from './new-item';
+
+export default function Page() {
+  return (
+    <main className="p-4">
+      <h1 className="text-lg font-bold mb-7 text-blue-800">Add New Item</h1>
+    </main>
+  );
+}
