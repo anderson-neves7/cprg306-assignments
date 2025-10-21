c0c889a1ab808abaf4486fa8fe6ceefdbe01a875
anderson-neves7 <anderson.neves@edu.sait.ca>
2025-10-20 23:12:45 -0600

    Add link to Week 6 page

--- app/page.js
+++ app/page.js
@@ -12,6 +12,8 @@ export default function Home() {
         <Link href="/week-4">Go to Week 4 Page</Link>
         <br />
         <Link href="/week-4">Go to Week 5 Page</Link>
+        <br />
+        <Link href="/week-6">Go to Week 6 Page</Link>
       </nav>
       <p>Welcome to my assignment hub. Click above to view Week 2.</p>
     </main>
