d74a942683d9fe933b57c22539e9ba9c4b4d33b0
anderson-neves7 <anderson.neves@edu.sait.ca>
2025-11-24 23:44:04 -0700

    Fixed firestore issue

--- app/utils/firebase.js
+++ app/utils/firebase.js
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyB867DHVhCkYBkA_KHnMVFbJmcW_INATjk",
+  authDomain: "cprg306-week10.firebaseapp.com",
+  projectId: "cprg306-week10",
+  storageBucket: "cprg306-week10.firebasestorage.app",
+  messagingSenderId: "134591568074",
+  appId: "1:134591568074:web:a471cb594a753961c87c33"
 };
 
 // Initialize Firebase
