f4160e17b5ba20005dd1065a0674a2ce550b3f9a
anderson-neves7 <anderson.neves@edu.sait.ca>
2025-11-24 22:38:43 -0700

    Updated landing page link to shopping list for week 10

--- app/week-10/page.js
+++ app/week-10/page.js
@@ -1,45 +1,11 @@
-"use client";
-
 import Link from "next/link";
-import { useUserAuth } from "../contexts/AuthContext";
 
 export default function Page() {
-  const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
-
   return (
-    <main className="p-4">
-      <h1 className="text-xl font-bold mb-4">Week 9: Firebase Auth</h1>
-
-      {/* Show login button if no user is signed in */}
-      {!user ? (
-        <button
-          onClick={gitHubSignIn}
-          className="bg-black text-white px-4 py-2 rounded"
-        >
-          Sign in with GitHub
-        </button>
-      ) : (
-        <>
-          {/* Show user info and logout if signed in */}
-          <p className="mb-2">Signed in as: {user.displayName}</p>
-          <button
-            onClick={firebaseSignOut}
-            className="bg-gray-200 px-4 py-2 rounded"
-          >
-            Sign out
-          </button>
-
-          {/* Link to shopping list */}
-          <div className="mt-4">
-            <Link
-              href="/week-9/shopping-list"
-              className="text-blue-600 underline"
-            >
-              Go to Shopping List
-            </Link>
-          </div>
-        </>
-      )}
+    <main>
+      <h1>Week 10</h1>
+      <p>Cloud FireStore + Shopping List</p>
+      <Link href="/week-10/shopping-list">Go to Shopping List</Link>
     </main>
   );
 }
